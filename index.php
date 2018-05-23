8fc5d58f740d8e3d0d70220b8358dc147eb59e4c
Kauã Semenov <kauagabrielsemenov@gmail.com>
2018-05-23 09:55:50 -0300

    index alteração

---
+++ index.php
@@ -0,0 +1,469 @@
+<?php include ("view/includes/cabecalho/cabecalho.php"); ?>
+	
+	<body>	  
+		<?php include ("view/includes/navbar/menu.php"); ?>
+
+		<section id="section_home" data-section="home" style="background-image: url(/view/assets/imagens/full_viagem_espaco.png);" data-stellar-background-ratio="0.5">
+			<div class="gradient"></div>
+			<div class="container">
+				<div class="text-wrap">
+					<div class="text-inner">
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 text-center">
+								<div class="call-to-action">
+									<a href="#" class="download to-animate"><i class="icon-download"></i> Download</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="fh5co-services" data-section="services">
+			<div class="fh5co-services">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 section-heading text-center">
+							<h2 class="to-animate"><span>Em uma galáxia muito distante...</span></h2>
+							<div class="row">
+								<div class="col-md-8 col-md-offset-2 subtext">
+									<h3 class="to-animate">O jogo viagem ao espaço oferece a você, jovem viajante, uma experiência única. Aprenda sobre os planetas do nosso sistema solar enquanto acompanha com nosso amigo, Bob Space, a espaço nave dele quebrou e os pedaços se espalharam pelo espaço, ele precisa recuperar as engrenagens para retornar pra casa. Embarque nessa! </h3>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3 text-center">
+							<div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-paper-plane"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Aprenda enquanto se diverte</h3>
+									<p>Vem jogar! Com o viagem ao espaço você aprende brincando, descubra coisas sobre os planetas que você nunca imaginou.</p>
+								</div>
+							</div>
+
+							<!-- <div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-energy"></i></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Desvende novos mundos</h3>
+									<p>Garanta que o Bob Space colete todas as engrenagens necessárias para consertar sua espaço nave enquanto se desafia pelos planetas do nosso sistema.</p>
+								</div>
+							</div> -->
+						</div>
+						<div class="col-md-3 text-center">
+							<div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-trophy"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Desvende novos mundos</h3>
+									<p>Garanta que o Bob Space colete todas as engrenagens necessárias para consertar sua espaço nave enquanto se desafia pelos planetas do nosso sistema.</p>
+								</div>
+							</div>
+
+							<!-- <div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-paper-plane"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Sleek and Smooth Animation</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div> -->
+						</div>
+						<div class="col-md-3 text-center">
+							<div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-people"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Pergunte ao seu professor</h3>
+									<p>Assim como um jedi, pergunte sempre ao seu mestre quando surgir alguma dúvida sobre qualquer informação dos planetas, assim como o mestre Yoda ele sempre vai te guiar.</p>
+								</div>
+							</div>
+
+							<!-- <div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-screen-desktop"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Looks Amazing on Devices</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div> -->
+						</div>
+						<div class="col-md-3 text-center">
+							<div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-screen-desktop"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Conta pra gente</h3>
+									<p>Não esquece de comentar aqui no site o que você achou sobre o jogo, qual a fase mais difícil? Quão longe você já viajou? A gente quer saber!</p>
+								</div>
+							</div>
+
+							<!-- <div class="box-services">
+								<div class="icon to-animate">
+									<span><i class="icon-key"></i></span>
+								</div>
+								<div class="fh5co-post to-animate">
+									<h3>Secure Account</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div> -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="core-features">
+				<div class="grid2 to-animate" style="background-image: url(/view/assets/imagens/full_image_5.jpg);">
+				</div>
+				<div class="grid2 fh5co-bg-color">
+					<div class="core-f">
+						<h2 class="to-animate">Core Features</h2>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="core">
+									<i class="icon-cloud-download to-animate-2"></i>
+									<div class="fh5co-post to-animate">
+										<h3>Free Download</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+								</div>
+								<div class="core">
+									<i class="icon-laptop to-animate-2"></i>
+									<div class="fh5co-post to-animate">
+										<h3>Responsive Layout</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+								</div>
+								<div class="core">
+									<i class="icon-hand-paper-o to-animate-2"></i>
+									<div class="fh5co-post to-animate">
+										<h3>24/7 Help &amp; Support</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="core">
+									<i class="icon-lightbulb-o to-animate-2"></i>
+									<div class="fh5co-post to-animate">
+										<h3>Free Update</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+								</div>
+								<div class="core">
+									<i class="icon-trophy to-animate-2"></i>
+									<div class="fh5co-post to-animate">
+										<h3>Featured Template</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+								</div>
+								<div class="core">
+									<i class="icon-columns2 to-animate-2"></i>
+									<div class="fh5co-post to-animate">
+										<h3>Lots of Elements</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="fh5co-explore" data-section="explore">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate"><span>Explore Our Projects</span></h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext to-animate">
+								<h3>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-project">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-4 text-center">
+							<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-1.jpg);">
+								<div class="desc">
+									<h3><a href="#">Work Title Here</a></h3>
+									<span>Web Design</span>
+
+									<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+									<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-2.jpg);">
+										<div class="desc">
+											<h3><a href="#">Work Title Here</a></h3>
+											<span>Web Design</span>
+
+											<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+											<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-5.jpg);">
+										<div class="desc">
+											<h3><a href="#">Work Title Here</a></h3>
+											<span>Web Design</span>
+
+											<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+											<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-9.jpg);">
+								<div class="desc">
+									<h3><a href="#">Work Title Here</a></h3>
+									<span>Web Design</span>
+
+									<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+									<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-4 text-center">
+							<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-4.jpg);">
+								<div class="desc">
+									<h3><a href="#">Work Title Here</a></h3>
+									<span>Web Design</span>
+
+									<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+									<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+								</div>
+							</div>
+							<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-3.jpg);">
+								<div class="desc">
+									<h3><a href="#">Work Title Here</a></h3>
+									<span>Web Design</span>
+
+									<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+									<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="project-grid to-animate-2" style="background-image:  url(view/imagens/project-10.jpg);">
+										<div class="desc">
+											<h3><a href="#">Work Title Here</a></h3>
+											<span>Web Design</span>
+
+											<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+											<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-11.jpg);">
+										<div class="desc">
+											<h3><a href="#">Work Title Here</a></h3>
+											<span>Web Design</span>
+
+											<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+											<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-4 text-center">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-6.jpg);">
+										<div class="desc">
+											<h3><a href="#">Work Title Here</a></h3>
+											<span>Web Design</span>
+
+											<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+											<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-7.jpg);">
+										<div class="desc">
+											<h3><a href="#">Work Title Here</a></h3>
+											<span>Web Design</span>
+
+											<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+											<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-8.jpg);">
+								<div class="desc">
+									<h3><a href="#">Work Title Here</a></h3>
+									<span>Web Design</span>
+
+									<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+									<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+								</div>
+							</div>
+							<div class="project-grid to-animate-2" style="background-image:  url(/view/assets/imagens/project-12.jpg);">
+								<div class="desc">
+									<h3><a href="#">Work Title Here</a></h3>
+									<span>Web Design</span>
+
+									<p class="download"><a href="#"><i class="icon-download"></i></a></p>
+									<p class="love"><a href="#"><i class="icon-heart-o"></i></a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+			
+		</section>
+		<section id="fh5co-testimony" class="fh5co-bg-color" data-section="testimony">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate"><span>Our Happy Clients</span></h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext to-animate">
+							</div>
+						</div>
+					</div>
+				</div> -->
+				<!-- <div class="row">
+					<div class="col-md-12 to-animate">
+						<div class="wrap-testimony">
+							<div class="owl-carousel-fullwidth">
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="/view/assets/imagens/person2.jpg" alt="user">
+										</figure>
+										<span>John Doe, via <a href="#" class="twitter">Twitter</a></span>
+										<blockquote>
+											<p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="/view/assets/imagens/person3.jpg" alt="user">
+										</figure>
+										<span>John Doe, via <a href="#" class="twitter">Twitter</a></span>
+										<blockquote>
+											<p>&ldquo;Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="/view/assets/imagens/person2.jpg" alt="user">
+										</figure>
+										<span>John Doe, via <a href="#" class="twitter">Twitter</a></span>
+										<blockquote>
+											<p>&ldquo;Far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="getting-started getting-started-1">
+			<div class="getting-grid" style="background-image:  url(/view/assets/imagens/full_image_3.jpg);">
+				<div class="desc">
+					<h2>Getting <span>Started</span></h2>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+				</div>
+			</div>
+			<a href="#" class="getting-grid2">
+				<div class="call-to-action text-center">
+					<p href="#" class="sign-up">Sign Up For Free</p>
+				</div>
+			</a>
+		</div>
+
+		<section id="fh5co-trusted" data-section="trusted">
+			<div class="fh5co-trusted">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 section-heading text-center">
+							<h2 class="to-animate"><span>Trusted By</span></h2>
+							<div class="row">
+								<div class="col-md-8 col-md-offset-2 subtext">
+									<h3 class="to-animate">We’re trusted by these popular companies</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						 <div class="col-md-2 col-sm-3 col-xs-6 col-sm-offset-0 col-md-offset-1">
+						 	<div class="partner-logo to-animate-2">
+						 		<img src="/view/assets/imagens/logo1.png" alt="Partners" class="img-responsive">
+						 	</div>
+						 </div>
+					    <div class="col-md-2 col-sm-3 col-xs-6">
+					    	<div class="partner-logo to-animate-2">
+					    		<img src="/view/assets/imagens/logo2.png" alt="Partners" class="img-responsive">
+							</div>
+					    </div>
+					    <div class="col-md-2 col-sm-3 col-xs-6">
+					    	<div class="partner-logo to-animate-2">
+					    		<img src="/view/assets/imagens/logo3.png" alt="Partners" class="img-responsive">
+					    	</div>
+					    </div>
+					    <div class="col-md-2 col-sm-3 col-xs-6">
+					    	<div class="partner-logo to-animate-2">
+					    		<img src="/view/assets/imagens/logo4.png" alt="Partners" class="img-responsive">
+					    	</div>
+					    </div>
+					    <div class="col-md-2 col-sm-12 col-xs-12">
+					    	<div class="partner-logo to-animate-2">
+					    		<img src="/view/assets/imagens/logo5.png" alt="Partners" class="img-responsive">
+					    	</div>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="getting-started getting-started-1">
+			<div class="getting-grid" style="background-image:  url(/view/assets/imagens/full_image_3.jpg);">
+				<div class="desc">
+					<h2>Getting <span>Started</span></h2>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+				</div>
+			</div>
+			<a href="#" class="getting-grid2">
+				<div class="call-to-action text-center">
+					<p href="#" class="sign-up">Sign Up For Free</p>
+				</div>
+			</a> -->
+		<!-- </div> -->
+
+		</body>
+
+		<?php include ("view/includes/rodape/rodape.php"); ?>
+
+
